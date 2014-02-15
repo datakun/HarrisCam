baa068707b961923cfbe67465b408b45d53b25ff
datakun <kimdatagoon@gmail.com>
2014-02-16 00:21:07 +0900

    HarrisCam Makeover 첫 커밋 #2

--- HarrisCam/jni/Android.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE    := imgeffects
-LOCAL_SRC_FILES := imgeffects.c
-LOCAL_LDLIBS    := -ljnigraphics
-
-include $(BUILD_SHARED_LIBRARY)
