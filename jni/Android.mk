bae39934cbd93f7d66d3212f4d4e26b01d5f9037
datakun <kimdatagoon@gmail.com>
2014-05-06 14:11:06 +0900

    Face Off. Look and feel almost changed.

--- jni/Android.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE    := HarrisCam
-LOCAL_SRC_FILES := HarrisCam.c
-LOCAL_LDLIBS    := -llog -ljnigraphics
-
-include $(BUILD_SHARED_LIBRARY)
